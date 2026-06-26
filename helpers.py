5a35cc9af562ccd631df401b76a6ab31b066914c
Daria Momatyuk <daria.momatyuk@yandex.ru>
2026-06-26 18:44:24 +0300

    Убраны выводы в консоль

--- helpers.py
+++ helpers.py
@@ -30,15 +30,11 @@ def get_login_data(data):
 def optional_cleanup(response, data=None):
     if response.status_code == 200:
         courier_id = response.json()
-        CourierMethods.delete_courier(courier_id['id'], courier_id)
-        print("Данные очищены.")
+        return CourierMethods.delete_courier(courier_id['id'], courier_id)
     elif response.status_code == 201:
         r = CourierMethods.login_courier(get_login_data(data))
         courier_id = r.json()
-        CourierMethods.delete_courier(courier_id['id'], courier_id)
-        print("Данные очищены.")
-    else: 
-        print("Данные не были добавлены, очистки не произошло.")
+        return CourierMethods.delete_courier(courier_id['id'], courier_id)
 
 def modify_data(data, key, value):
     modified_data = data.copy()
@@ -47,15 +43,9 @@ def modify_data(data, key, value):
 
 def optional_cancel_order(response):
     if response.status_code == 201:
-        OrderMethods.cancel_order(response.json())
-        print("Заказ отменен.")
-    else:
-        print("Заказ не был создан.")
+        return OrderMethods.cancel_order(response.json())
 
 def optional_finish_order(in_delivery, id, id_data):
     if in_delivery == True:
-        OrderMethods.finish_order(id, id_data)
-        print("Заказ завершен.")
-    else:
-        print("Заказ не может быть завершен.")
+        return OrderMethods.finish_order(id, id_data)
 
